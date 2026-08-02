6a6a858ab3de8484e4ce9d0eef8360e12444cf1e
batul02 <batulmakada2000@gmail.com>
2026-08-02 14:01:19 +0530

    testing auth

---
+++ backend/tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+@pytest.fixture
+def client():
+    """
+    Returns a FastAPI TestClient.
+    """
+    with TestClient(app) as test_client:
+        yield test_client
